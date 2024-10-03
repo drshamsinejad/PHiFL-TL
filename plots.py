61402f53b46ac8d4fe2be80ebdd8efa6ad49c555
Afsaneh Afzali <af.afzali@sutech.ac.ir>
2024-10-03 09:29:51 +0330

    Update plots.py

--- plots.py
+++ plots.py
@@ -20,6 +20,13 @@ def client_plot(client,folder):
         elif y[i][1]==2:     # level 2 : in cloud server
             plt.plot(i,y1[i], color="green", marker='^',label=l2)
             l2="_nolegend_"
+    x_cloud=[]
+    y_cloud=[]
+    for i in x:
+        if y[i][1]==0:         # level 0 : in cloud server
+            x_cloud.append(i)
+            y_cloud.append(y[i][0])
+    plt.plot(x_cloud,y_cloud, linewidth='1',linestyle='--',color='purple')
     plt.xticks(x)
     plt.xticks(visible=False)
     plt.legend()
