a58be07fbc2385316fcc8771a96e23c09f93e462
Afsaneh Afzali <af.afzali@sutech.ac.ir>
2024-10-21 09:40:14 +0330

    Update initialize_model.py

--- models/initialize_model.py
+++ models/initialize_model.py
@@ -3,14 +3,14 @@ from model.cnn import CNN_1
 from model.cnn import CNN_2
 from model.cnn import CNN_3
 
-def create(dataset,model,loss,metrics,lr,image_shape):  
+def create(dataset,model,loss,metrics,lr,image_shape,num_labels):  
     
     #if dataset=="mnist":
      #   if model=="mlp":
       #      m=SimpleMLP(784,10,loss,metrics,lr)
             
     if model=='cnn1':
-        m=CNN_1(loss,metrics,lr,image_shape)
+        m=CNN_1(loss,metrics,lr,image_shape,num_labels)
             
     elif model=='cnn2':
         m=CNN_2(loss,metrics,lr,image_shape)
