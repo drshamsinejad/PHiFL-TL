ac69b1e977aeed9d372dc85e00b502a07268eb75
Afsaneh Afzali <af.afzali@sutech.ac.ir>
2024-10-21 09:38:33 +0330

    Update cnn.py

--- models/cnn.py
+++ models/cnn.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense
 import tensorflow as tf
 
 
-def CNN_1(loss,metrics,lr,image_shape):     
+def CNN_1(loss,metrics,lr,image_shape,num_labels):     
    
     model = Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=image_shape))
@@ -14,14 +14,14 @@ def CNN_1(loss,metrics,lr,image_shape):
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.Flatten())
     model.add(Dense(64, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(num_labels, activation='softmax'))
     #adam_opt=tf.keras.optimizers.Adam(learning_rate=lr)
     pt = tf.keras.optimizers.SGD(learning_rate=lr)
     model.compile(loss=loss,metrics=metrics,optimizer=pt)
     #print(model.summary())    
     return model
 
-def CNN_2(loss,metrics,lr,image_shape):           
+def CNN_2(loss,metrics,lr,image_shape,num_labels):          
 
     model = Sequential()
     model.add(layers.Conv2D(32, (5, 5), activation='relu',padding='same', input_shape=image_shape))
@@ -30,13 +30,13 @@ def CNN_2(loss,metrics,lr,image_shape):
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
     model.add(Dense(512, activation='relu'))
-    model.add(Dense(10, activation='softmax'))  
+    model.add(Dense(num_labels, activation='softmax'))  
     adam_opt=tf.keras.optimizers.Adam(learning_rate=lr)
     model.compile(loss=loss,metrics=metrics,optimizer=adam_opt)
     
     return model
 
-def CNN_3(loss,metrics,lr,image_shape):  
+def CNN_3(loss,metrics,lr,image_shape,num_labels):   
     
     model = Sequential()
     model.add(layers.Conv2D(6, (5, 5), activation='relu',padding='same', input_shape=image_shape))
@@ -46,7 +46,7 @@ def CNN_3(loss,metrics,lr,image_shape):
     model.add(layers.Flatten())
     model.add(Dense(120, activation='relu'))
     model.add(Dense(84, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(num_labels, activation='softmax'))
     adam_opt=tf.keras.optimizers.Adam(learning_rate=lr)
     model.compile(loss=loss,metrics=metrics,optimizer=adam_opt)
     
